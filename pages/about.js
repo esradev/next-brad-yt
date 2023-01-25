ddf6b9cf14baee3bb5fc556142eb58eebd3b65f8
Wpstorm <wpstormdev@gmail.com>
2023-01-25 11:02:02 +0330

    first commit!

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+export default function About(props) {
+  return (
+    <>
+      <h2>About Us</h2>
+      <p>
+        Welcome to this amazing about page. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Porro dolore officiis atque voluptas,
+        quas, repellendus cum, magnam a alias unde reiciendis voluptates aliquam
+        maxime laborum? Quae omnis eius impedit et?
+      </p>
+      <p>I have {props.repoCount} public repos on GitHub.</p>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://api.github.com/users/esradev");
+  const data = await response.json();
+
+  return {
+    props: {
+      repoCount: data.public_repos,
+    },
+    revalidate: 10,
+  };
+}
